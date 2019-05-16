e26d1ad777f74bcf45616d4efb15200444ceada5
Hung Nguyen Viet <viethung.nguyen@grabtaxi.com>
2019-05-16 13:34:37 +0700

    Add user recipe

---
+++ recipes/add_user.rb
@@ -0,0 +1,11 @@
+user "Add a user" do
+  home "/home/fabian"
+  shell "/bin/bash"
+  username "fabian"
+end
+
+user "Add another user" do
+  home "/home/hvn"
+  shell "/bin/bash"
+  username "hung"
+end
